6cb8c36ec0efc0bcd1daefb4f9719dde975bfa46
Vaibhav Verma <vaibhavverma9@gmail.com>
2023-10-31 08:20:53 -0700

    fixing issue with handleChangeText useEffect

--- GooglePlacesAutocomplete.js
+++ GooglePlacesAutocomplete.js
@@ -171,7 +171,7 @@ export const GooglePlacesAutocomplete = forwardRef((props, ref) => {
       _abortRequests();
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [stateText]);
+  }, []);
 
   useEffect(() => {
     // Update dataSource if props.predefinedPlaces changed
